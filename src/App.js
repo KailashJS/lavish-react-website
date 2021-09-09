bfce8f74bcc2f5ff653623b31b46204d428a0c18
Kailash Chandra Bhakta <kcbhakta.99@gmail.com>
2021-09-09 22:13:16 +0530

    Router, Icons and Toggle button Navbar for mobile viewport implemented - WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Brand Lavish</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { MdFingerprint } from "react-icons/md";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            <MdFingerprint className="navbar-icon" />
+            LAVISH
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
