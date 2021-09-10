2811623aa3700af37730012f8908a41eeb77dfed
Kailash Chandra Bhakta <kcbhakta.99@gmail.com>
2021-09-10 12:47:34 +0530

    NavBar Styling Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,25 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MdFingerprint } from "react-icons/md";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Button } from "./Button";
+import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(false)
+    }
+  }
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -19,6 +33,34 @@ function Navbar() {
           <div className="menu-icon" onClick={handleClick}>
             {click ? <FaTimes /> : <FaBars />}
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-links">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/products" className="nav-links">
+                Products
+              </Link>
+            </li>
+            <li className="nav-btn">
+              {button ? (
+                <Link to='/sign-up' className="btn-link">
+                  <Button buttonStyle='btn--outline'>SIGN UP</Button>
+                </Link>
+              ): (
+                <Link tp='/sign-up' className="btn-link">
+                  <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>SIGN UP</Button>
+                </Link>
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </>
