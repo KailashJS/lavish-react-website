0dafa24fced222b2dfac73c9b068b43eabfda18c
Kailash Chandra Bhakta <kcbhakta.99@gmail.com>
2021-09-21 00:01:05 +0530

    Hero Section with SVG and dark background added/implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from './components/pages/HomePage/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" />
+        <Route path="/" exact component={Home}/>
       </Switch>
     </Router>
   );
